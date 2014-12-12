678b84d9b9618a7ad82742dd396f9a481ddcbf9d
magelinha <joao.dk16@gmail.com>
2014-12-12 00:08:10 -0200

    Q-rota
    Q-rota executando com sucesso

---
+++ Q3/file_reader.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 14 17:29:38 2014
+
+@author: LuizF
+"""
+
+import xml.etree.ElementTree as ET
+from os import listdir
+from os.path import isfile, join, splitext
+from re import findall
+
+class FileReader:
+    """Class to read .xml input files"""
+
+    # Fill file list in sorted order by input size
+    def __init__(self):
+        self.distMatrix = []
+        self.currentId = None
+        self.basePath = '..\\'
+        print (listdir(self.basePath))
+        self.fileList = [
+            f for f in listdir(self.basePath)
+                if isfile(join(self.basePath,f))
+                    if splitext(f)[1] == '.xml'
+        ]
+        print(len(self.fileList))
+        self._ProcessFileList()
+
+    # Open file and fill adjacency matrix self.distMatrix
+    # File id's goes from 0 to 10
+    def OpenFile(self, id):
+        self.currentId = id
+        if id < len(self.fileList):
+            filePath = self.basePath + '\\' + self.fileList[id][1]
+            tree = ET.parse(filePath)
+            root = tree.getroot()
+            i = 0
+            for vertex in root.find('graph'):
+                self.distMatrix.append([])
+                j = 0
+                for d in vertex:
+                    if i == j:
+                        self.distMatrix[i].append(None)
+                    tmpValue = int(float(d.attrib.get('cost')))
+                    self.distMatrix[i].append(tmpValue)
+                    j+=1
+                i+=1
+            self.distMatrix[i-1].append(None) # Add None to last element
+
+    def GetDistanceMatrix(self):
+        return self.distMatrix
+    def GetElemNumber(self):
+        return self.fileList[self.currentId][0]
+
+
+    # Sort files list by input size
+    def _ProcessFileList(self):
+        tmp = []
+        for f in self.fileList:
+            tmp.append(((int(findall(r'\d+', f)[0])),f))
+        tmp.sort()
+        self.fileList = tmp.copy()
+
+
+#i = 0
+#for vertex in root.find('graph'):
+#    print(i, vertex)
+#    for d in vertex:
+#        print(int(float(d.attrib.get('cost'))), end=' ')
+#    i+=1
--- Q3/q31.py
+++ Q3/q31.py
@@ -7,10 +7,12 @@
 	Implementação do q-rota
 """
 
-import sys,heapq,copy
+import sys,copy
+from heapq import heappush, heappop
 from MGraph import Graph
+import file_reader
 
-minTour = sys.maxint
+minTour = sys.maxsize
 
 class Node:
 	#deve ser informado o último vértice do conjunto já visitado
@@ -22,27 +24,31 @@ class Node:
 		self.path = []
 		self.S = []
 
+	def __lt__(self, other):
+		return self.bound < other.bound
+
 class TspBB:
 	def __init__(self, graph):
 		self.graph = graph #grafo a ser usado
-		self.infity = sys.maxint #simulação de um valor infinito
+		self.infity = sys.maxsize #simulação de um valor infinito
 		self.sizeGraph = len(graph.nodes) #quantidade de itens do grafo
 		self.heap = [] #simula um heap binário
-		self.minTour = sys.maxint
+		self.minTour = sys.maxsize
 		self.bestTour = []
 
 	def tsp(self):
 		while self.heap:
-			Node temp = heappop(self.heap)
+			temp = heappop(self.heap)
 
 			if(temp.bound < self.minTour):
-				for nextVertex in self.graph.edges[temp.lastVertex] #gera um novo nível com todos os vizinhos do item, desconsiderando os que já foram visitados
+				#gera um novo nível com todos os vizinhos do item, desconsiderando os que já foram visitados
+				for nextVertex in self.graph.edges[temp.lastVertex]: 
 					if nextVertex not in temp.path: #desconsidera se o vértice já faz parte da permutação parcial
 						
 						#cria um novo nó para o heap, adicionado o nível, tamanho e caminho atual
-						Node newNode = new Node(nextVertex)
+						newNode = Node(nextVertex)
 						newNode.level = temp.level + 1
-						newNode.length = temp.length + self.graph.distance[(temp.lastVertex, nextVertex)]
+						newNode.length = temp.length + self.graph.distances[(temp.lastVertex, nextVertex)]
 						
 						#lista com a caminho atual já percorrido
 						newNode.path = copy.deepcopy(temp.path)
@@ -54,14 +60,14 @@ class TspBB:
 
 						#executado quando falta apenas um 1 item para adicionar no path
 						if(newNode.level == self.sizeGraph - 2):
-							for node in self.graph.edges(nextVertex):
+							for node in self.graph.edges[nextVertex]:
 								if node not in newNode.path:
 									newNode.path.append(node)
-									newNode.length = newNode.length + self.graph.distance[(nextVertex, node)]
+									newNode.length = newNode.length + self.graph.distances[(nextVertex, node)]
 
 									#completa o tour, voltando para o nó inicial
-									newNode.append(newNode.path[0])
-									newNode.length = newNode.length + self.graph.distance[(node, newNode.path[0])]
+									newNode.path.append(newNode.path[0])
+									newNode.length = newNode.length + self.graph.distances[(node, newNode.path[0])]
 
 									if newNode.length < self.minTour:
 										self.minTour = newNode.length
@@ -70,7 +76,7 @@ class TspBB:
 									break
 						#o else indica que é um tour parcial, então deve ser calculado o lower bound
 						else:
-							newNode.bound = self.lowerBoundQRota(nextVertex, len(temp.S) - 1, temp.lastVertex, temp.S, self.graph)
+							newNode.bound = self.lowerBoundQRota(nextVertex, len(temp.S) - 1, temp.lastVertex, temp.S)
 							if newNode.bound < self.minTour:
 								heappush(self.heap, newNode)
 
@@ -81,15 +87,46 @@ class TspBB:
 		S = conjunto
 		v = ultimo elemento da permutação parcial
 	"""
-	def lowerBoundQRota(w, k, v ,S, graph):
+	def lowerBoundQRota(self, w, k, v, S):
 		resultados = []
-		menor = sys.maxint
+		menor = sys.maxsize
 
 		if k == 0:
-			return graph.distance[(v,w)]
+			return self.graph.distances[(v,w)]
 		for node in S:
-			menor = min(menor, graph.distance[(node, w)]  + lowerBoundQRota(node, k-1, v, S))
-		return menor + graph.distance[(w, 0)]
+			menor = min(menor, graph.distances[(node, w)]  + self.lowerBoundQRota(node, k-1, v, S))
+		return menor + graph.distances[(w, 0)]
+
+
+def createGraph(id):
+        graph = Graph()
+
+        """
+        reader = file_reader.FileReader()
+        reader.OpenFile(id)
+        dimension = reader.GetElemNumber()
+        distancesMatrix = reader.GetdistancesMatrix().copy()
+        """
+        dimension = 5
+        dist = [None]*dimension
+        dist[0] = [None,3,1,5,8]
+        dist[1] = [3,None,6,7,9]
+        dist[2] = [1,6,None,4,2]
+        dist[3] = [5,7,4,None,3]
+        dist[4] = [8,9,2,3,None]
+        distanceMatrix = dist.copy()
+        
+        for i in range(dimension):
+        	graph.add_node(i)
+
+        for i in range(len(distanceMatrix)):
+        	for j in range(len(distanceMatrix[i])):
+        		if i==j:
+        			graph.add_edge(i,j,0)
+        		else:
+        			graph.add_edge(i,j,distanceMatrix[i][j])
+
+        return graph
 
 if __name__ == "__main__":
 
@@ -97,14 +134,15 @@ if __name__ == "__main__":
 	Aqui deve ser criado o grafo a ser passado para o tsp
 	"""
 
-	tspBB = new TspBB(graph)
+	graph = createGraph(0)
+	tspBB = TspBB(graph)
 	root = Node(0) #o vértice inicial sempre será o 0
 	root.path.append(0)
 	root.S = copy.deepcopy(graph.nodes)
 	root.S.remove(0)
-	root.bound = tspBB.lowerBoundQRota(0, len(root.S)-1, 0, root.S, tspBB.graph)
+	root.bound = tspBB.lowerBoundQRota(0, len(root.S)-1, 0, root.S)
 
 	heappush(tspBB.heap, root)
 	tspBB.tsp()
 
-	print tspBB.minTour #resultado final
+	print (tspBB.minTour) #resultado final
